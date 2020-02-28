648aa58f130b294f07cc70b8dd1f65be6ee5693e
jgrantps <jgrantps@hotmail.com>
2020-02-28 11:14:53 -0400

    secondary commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function App() {
+  return (
+    // <div className="App">
+    //   code goes here!
+    // </div>
+
+    <div className="max-w-sm m-10 rounded overflow-hidden shadow-lg">
+      <img className="w-full" src="https://avatars1.githubusercontent.com/u/50372930?s=460&v=4" alt="Display" />
+      <div className="">
+        <div className="">
+          Peter Grant
+        </div>
+        <p className="">
+          I'm trying my hardest, here. <span></span>😜
+        </p>
+      </div>
+      <div className="">
+        <span className="text-2xl bg-gray-800 text-white"># Junior Software Engineer</span>
+        <span className="">#Archvis nerd</span>
+        <span className="">#Father and Husband</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
